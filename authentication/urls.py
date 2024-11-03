46a51b948354623691d032b1c83cafe2374fa246
Arnavongit <arnavtrivediofficial@gmail.com>
2024-11-04 02:22:55 +0530

    Initial Commit

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('signup', views.signup, name="signup"),
-    path('', views.signin, name="signin"),
-    path('', views.signout, name="signout"),
+    path('signup/', views.signup, name="signup"),
+    path('signin/', views.signin, name="signin"),  # This should match the 'signin' name in the form action
+    path('signout/', views.signout, name="signout"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, "authentication/index.html")
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+        # Check if passwords match
+        # if pass1 != pass2:
+        #     messages.error(request, "Passwords do not match")
+        #     return redirect('signup')
+        
+        # Create the user
+        myuser = User.objects.create_user(username=username, email=email, password=pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        
+        messages.success(request, "Your account has been successfully created")
+        
+        print("User created successfully, redirecting to signin...")  # Debug statement
+        return redirect('signin')  # Ensure this is a URL pattern name, not a template name
+    
     return render(request, "authentication/signup.html")
 
+        
 def signin(request):
     return render(request, "authentication/signin.html")
 
